a3caa526e0a2c665b98bbcaae6719cb617b4540c
Nobeen <n.islam.1@outlook.com>
2022-04-01 09:06:49 +0100

    Added trashList and filtered main list according to trash

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import {useState} from "react"
+import { useState } from "react";
 
-interface task{
-  id: "string"
-  taskName: "string"
+// interface task{
+//   id: "string"
+//   taskName: "string"
 
-}
+// }
 
 function App(): JSX.Element {
-  const [taskList,setTaskList] = useState<string[]>([])
-  const [taskInput,setTaskInput]=useState<string>("")
+  const [taskList, setTaskList] = useState<string[]>([]);
+  const [taskInput, setTaskInput] = useState<string>("");
+  const [trashList, setTrashList] = useState<string[]>([]);
 
-  const taskListElements =taskList.map((task,index)=> <li key = {index}>{task}</li>)
+  const filteredTaskList = taskList.filter((task) => !trashList.includes(task));
+  const taskListElements = filteredTaskList.map((task, index) => {
+    return (
+      <>
+        <li key={index}>{task}</li>
+        <button
+          onClick={() => {
+            setTrashList((prevTrashList) => [...prevTrashList, task]);
+          }}
+        >
+          Delete Task
+        </button>
+      </>
+    );
+  });
 
   return (
-  <>
-
-    <h1>Nobeen's TodoList</h1>
-    <input 
-      placeholder="Type to create task"
-      value = {taskInput}
-      onChange = {(event)=>{
-        setTaskInput(event.target.value)
-      }}
-    >
-    </input>
-    <button
-      onClick = {() => {
-        setTaskList(prevtaskList => [...prevtaskList, taskInput])
-      }}
-    >Creat Task</button>
-    <ol>{taskListElements}</ol>
-
-  </>
-  )
+    <>
+      <h1>Nobeen's TodoList</h1>
+      <input
+        placeholder="Type to create task"
+        value={taskInput}
+        onChange={(event) => {
+          setTaskInput(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          setTaskList((prevtaskList) => [...prevtaskList, taskInput]);
+        }}
+      >
+        Create Task
+      </button>
+      <ol>{taskListElements}</ol>
+      <p>{trashList}</p>
+    </>
+  );
 }
 
 export default App;
