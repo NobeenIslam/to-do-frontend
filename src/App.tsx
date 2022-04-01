791d06830af4499e71844ffb4aef2e5cf5f1ed8a
Nobeen <n.islam.1@outlook.com>
2022-04-01 13:07:39 +0100

    Removed completed list and trash list. Only create button remains with working post to back end

--- src/App.tsx
+++ src/App.tsx
@@ -14,30 +14,12 @@ const baseUrl =
 function App(): JSX.Element {
   const [taskList, setTaskList] = useState<string[]>([]);
   const [taskInput, setTaskInput] = useState<string>("");
-  const [completedList, setCompletedList] = useState<string[]>([]);
-  const [trashList, setTrashList] = useState<string[]>([]);
 
-  const filteredTaskList = taskList
-    .filter((task) => !trashList.includes(task))
-    .filter((task) => !completedList.includes(task));
+  const filteredTaskList = taskList;
   const taskListElements = filteredTaskList.map((task, index) => {
     return (
       <section key={index}>
         <li>{task}</li>
-        <button
-          onClick={() => {
-            setTrashList((prevTrashList) => [...prevTrashList, task]);
-          }}
-        >
-          Delete Task
-        </button>
-        <button
-          onClick={() => {
-            setCompletedList((prevCompleteList) => [...prevCompleteList, task]);
-          }}
-        >
-          Complete Task
-        </button>
       </section>
     );
   });
@@ -55,12 +37,6 @@ function App(): JSX.Element {
       <button
         onClick={() => {
           axios.post(baseUrl + "/tasks", { taskName: taskInput });
-          //axios.get(baseUrl+"/").then((response) => response.json())
-          fetch(baseUrl + "/")
-            .then((response) => response.json())
-            .then((jsonBody) => {
-              console.log(jsonBody[1].taskName);
-            });
           setTaskList((prevtaskList) => [...prevtaskList, taskInput]);
         }}
       >
@@ -68,9 +44,6 @@ function App(): JSX.Element {
       </button>
       <h3>To do:</h3>
       <ol> {taskListElements}</ol>
-      <p>Trash: {trashList.join(", ")}</p>
-      <p>Completed: {completedList.join(", ")}</p>
-      <p>{baseUrl + "/tasks"}</p>
     </>
   );
 }
