d0bb9bd722dd8057d58accf62d3c9243c4e4ad37
Nobeen <n.islam.1@outlook.com>
2022-04-01 12:14:59 +0100

    Imported axios. Added key to section tag in task list map to remove key error. Added post request to creat task button. Get request (naive).

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import axios from "axios";
 
-// interface task{
-//   id: "string"
-//   taskName: "string"
+interface task {
+  id: "string";
+  taskName: "string";
+}
 
-// }
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "your-project.herokuapp.com"
+    : "http://localhost:4000";
 
 function App(): JSX.Element {
   const [taskList, setTaskList] = useState<string[]>([]);
@@ -17,8 +22,8 @@ function App(): JSX.Element {
     .filter((task) => !completedList.includes(task));
   const taskListElements = filteredTaskList.map((task, index) => {
     return (
-      <>
-        <li key={index}>{task}</li>
+      <section key={index}>
+        <li>{task}</li>
         <button
           onClick={() => {
             setTrashList((prevTrashList) => [...prevTrashList, task]);
@@ -33,7 +38,7 @@ function App(): JSX.Element {
         >
           Complete Task
         </button>
-      </>
+      </section>
     );
   });
 
@@ -49,6 +54,13 @@ function App(): JSX.Element {
       ></input>
       <button
         onClick={() => {
+          axios.post(baseUrl + "/tasks", { taskName: taskInput });
+          //axios.get(baseUrl+"/").then((response) => response.json())
+          fetch(baseUrl + "/")
+            .then((response) => response.json())
+            .then((jsonBody) => {
+              console.log(jsonBody[1].taskName);
+            });
           setTaskList((prevtaskList) => [...prevtaskList, taskInput]);
         }}
       >
@@ -58,6 +70,7 @@ function App(): JSX.Element {
       <ol> {taskListElements}</ol>
       <p>Trash: {trashList.join(", ")}</p>
       <p>Completed: {completedList.join(", ")}</p>
+      <p>{baseUrl + "/tasks"}</p>
     </>
   );
 }
