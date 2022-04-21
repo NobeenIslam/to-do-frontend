e11f3dbd740f81a1c05dbddaa8de3e30e39994dd
Nobeen <n.islam.1@outlook.com>
2022-04-21 12:10:10 +0100

    Created conditional render for edit task button

--- src/components/TaskElement.tsx
+++ src/components/TaskElement.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { taskElementProps } from "./types";
 import axios from "axios";
 
 export function TaskElement(props: taskElementProps): JSX.Element {
+  const [inputToEditTask, setInputToEditTask] = useState<string>("")
+  const [isEditTaskSelected, setIsEditTaskSelected] = useState<boolean>(false)
+
+  function handleClickEditTask() {
+    setIsEditTaskSelected(true)
+  }
+
+  function handleSubmitEditTask() {
+    setIsEditTaskSelected(false)
+  }
+
   function decrementCounter(counter: number) {
     return counter - 1;
   }
@@ -17,6 +29,27 @@ export function TaskElement(props: taskElementProps): JSX.Element {
       >
         Delete Task
       </button>
+      <button
+        onClick={handleClickEditTask}
+      >
+        Edit Task
+      </button>
+      {isEditTaskSelected &&
+        <>
+          <input
+            placeholder="Edit Task"
+            value={inputToEditTask}
+            onChange={(event) => setInputToEditTask(event.target.value)}
+          >
+          </input>
+          <button
+            onClick={handleSubmitEditTask}
+          >
+            Submit Edit
+          </button>
+        </>
+
+      }
     </section>
   );
 }
